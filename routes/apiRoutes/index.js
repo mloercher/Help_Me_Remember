7e08a02f0456807dc258151886497eb683b4bd10
Mark <markloercher@gmail.com>
2021-12-18 13:05:15 -0500

    cleaned up routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const database = require("mime-db");
 const db = require("../../db/db.json");
 const uuid = require("../../public/assets/js/uuid");
+
 // `GET /api/notes`
 router.get("/notes", (req, res) => {
     console.log(req);
@@ -11,17 +12,6 @@ router.get("/notes", (req, res) => {
 
 // `POST /api/notes` 
 router.post("/notes", (req,res) => {
-  // console.log("post is working")
-  // fs.readFileSync(db,(err, data) => {
-  //   if(err) throw err;
-  //   var newNote = {...req.body, id: uuid()}
-  //   var notesData = JSON.parse(data));
-  //   console.log(notesData);
-  //   var updatedNotes = [...notesData, newNote]
-  //   fs.writeFileSync(db, JSON.stringify(updatedNotes))
-  //   res.json(newNote);
-
-  // })
   if(!req.body.id) {
     req.body.id = uuid();
   }
@@ -29,7 +19,8 @@ router.post("/notes", (req,res) => {
   fs.writeFileSync('../../db/db.json', JSON.stringify(db));
   res.json(req.body);
 })
-// // DELETE /api/notes/:id
+
+// DELETE /api/notes/:id
 // router.delete("/api/notes/:id", (req,res) => {
 //   var id = req.params.id; 
 // fs.readFileSync()
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,12 +1,7 @@
 const path = require("path");
 const router = require("express").Router();
 
-
 //route to index
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, '../../public.index.html'));
-// })
-
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
