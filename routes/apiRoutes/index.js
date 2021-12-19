56961770953543609e441f15c4198bd4cb115a94
Mark <markloercher@gmail.com>
2021-12-19 11:42:10 -0500

    fixed 500 error on api/notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const database = require("mime-db");
+// const database = require("mime-db");
 const db = require("../../db/db.json");
 const uuid = require("../../public/assets/js/uuid");
+const fs = require('fs');
 
 // `GET /api/notes`
 router.get("/notes", (req, res) => {
@@ -15,16 +16,24 @@ router.post("/notes", (req,res) => {
   if(!req.body.id) {
     req.body.id = uuid();
   }
-  db.push(req.body);
-  fs.writeFileSync('../../db/db.json', JSON.stringify(db));
+  db.push(req.body);                    
+  fs.writeFileSync('./db/db.json', JSON.stringify(db));
   res.json(req.body);
 })
 
-// DELETE /api/notes/:id
+// // DELETE /api/notes/:id
 // router.delete("/api/notes/:id", (req,res) => {
-//   var id = req.params.id; 
-// fs.readFileSync()
-// fs.writeFileSync()
+//   var id = req.params.id;
+//   fs.readFileSync('../../db/db.json', JSON.parse(db));
+//   if (req.params.id !== id) {
+//     fs.writeFileSync('../../db/db.json', JSON.stringify(db));
+//     res.json(req.body);
+//   }
+  
+  
+
+
+
 // })
 
 module.exports = router;
